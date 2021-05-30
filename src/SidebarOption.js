d931cdf1a720560fa3a20044b7e2ecac29949874
Israr <abu.israr.khan.9@gmail.com>
2021-05-31 01:26:29 +0530

    updated till sidebarOption

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './SidebarOption.css';
+import './Sidebar.js'
+
 
 function SidebarOption() {
     return (
-        <div>
+        <div className="sidebarOption">
+            <h2>Home</h2>
             
         </div>
     )
