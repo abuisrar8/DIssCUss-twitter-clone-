5d6635d4ede8d2ddf75656bc860baf0a6f2479f9
Israr <abu.israr.khan.9@gmail.com>
2021-05-31 21:21:24 +0530

    updated till Feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Feed.css';
+import TweetBox from './TweetBox';
+import Post from './Post';
 
 function Feed() {
     return (
-        <div className="Feed">
+        <div className="feed">
             {/* {Header} */}
+            <div className = "feed__header">
             <h2>Home</h2>
+            </div>          
+
 
             {/* {tweetbox} */}
-
+            <TweetBox/>
             {/* {post} */}
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
             {/* {post} */}
             {/* {post} */}
             {/* {post} */}
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -5,10 +5,13 @@ import './Sidebar.js'
 
 function SidebarOption({active,Icon, text}) {
     return (
+        <div className="sidebarInner">
         <div className={`sidebarOption ${active && 'SidebarOption--active'}`}>
+            
             <Icon/>
             <h2>{text}</h2>
             
+        </div>
         </div>
     )
 }
